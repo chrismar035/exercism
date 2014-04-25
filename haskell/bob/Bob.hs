346d8ed7f2218f34872438bce471c9af136480a9
Chris Marshall <chris@chrismar035.com>
2014-04-24 23:30:46 -0400

    Haskell: Bob

--- haskell/bob/Bob.hs
+++ haskell/bob/Bob.hs
@@ -1,4 +1,18 @@
 module Bob (responseFor) where
+  import Data.String.Utils
+  import Data.Char
 
-responseFor :: String -> String
-responseFor = undefined
+  responseFor :: String -> String
+  responseFor request | strip request == "" = "Fine. Be that way!"
+                      | (endswith "?" request) = "Sure."
+                      | isShout request = "Woah, chill out!"
+                      | otherwise = "Whatever."
+                      where
+                        isShout :: String -> Bool
+                        isShout s = allUpper s && not (isNumeric s)
+
+                        allUpper :: String -> Bool
+                        allUpper s = s == map toUpper s
+
+                        isNumeric :: String -> Bool
+                        isNumeric s = all isNumber s
